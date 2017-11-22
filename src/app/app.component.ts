b67676e788ce731c7140d8b80b2c3100ad4d8885
Marco Aurelio Soares <maurelio@avenuecode.com>
2017-11-22 07:52:46 -0200

    setting up angular universal branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDGDTtGNC4FkKpSe85qb2wLHzNi2xkxFy8",
-      authDomain: "ng-recipe-book.firebaseapp.com"
+      apiKey: "AIzaSyBrkKleAX_8jHpPmTchVBmDD7Hkj8TT1VE",
+      authDomain: "ng-recipe-book-3adbb.firebaseapp.com"
     });
   }
 
